bbedde344e99df89acfc285bbb736d33f69a3aef
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2019-01-31 19:38:00 +0100

    include SaiTub interface

---
+++ contracts/lib/sai/ITub.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.5.0;
+
+contract ISaiTub {
+    function open() public returns (bytes32);
+    function join(uint) public;
+    function exit(uint) public;
+    function lock(bytes32, uint) public;
+    function free(bytes32, uint) public;
+    function draw(bytes32, uint) public;
+    function wipe(bytes32, uint) public;
+    function give(bytes32, address) public;
+    function shut(bytes32) public;
+    function bite(bytes32) public;
+    function cups(bytes32) public returns (address, uint, uint, uint);
+    function gem() public returns (TokenInterface);
+    function gov() public returns (TokenInterface);
+    function skr() public returns (TokenInterface);
+    function sai() public returns (TokenInterface);
+    function vox() public returns (VoxInterface);
+    function ask(uint) public returns (uint);
+    function mat() public returns (uint);
+    function chi() public returns (uint);
+    function ink(bytes32) public returns (uint);
+    function tab(bytes32) public returns (uint);
+    function rap(bytes32) public returns (uint);
+    function per() public returns (uint);
+    function pip() public returns (PipInterface);
+    function pep() public returns (PepInterface);
+    function tag() public returns (uint);
+
+    function lad(bytes32 cup) external view returns (address) {
+      return msg.sender;
+    }
+
+    function drip() public;
+}
+
+contract TokenInterface {
+    function allowance(address, address) public returns (uint);
+    function balanceOf(address) public returns (uint);
+    function approve(address, uint) public;
+    function transfer(address, uint) public returns (bool);
+    function transferFrom(address, address, uint) public returns (bool);
+    function deposit() public payable;
+    function withdraw(uint) public;
+}
+
+contract VoxInterface {
+    function par() public returns (uint);
+}
+
+contract PipInterface {
+    function read() public returns (bytes32);
+}
+
+contract PepInterface {
+    function peek() public returns (bytes32, bool);
+}
